de4533720a4b63cc02a5534e9d15e91638d75464
George Tang <georgecctang@gmail.com>
2020-10-06 22:26:51 +0000

    implement char counter and tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  console.log('document ready...');
+  $(".new-tweet textarea").on('keyup', function() {
+    
+    // Traverse through the DOM to get the counter element
+    let counter = $(this).parents().children('footer').children('output');
+
+    // Calculate input length and remaining length
+    let inputLength = $(this).val().length;
+    let remainingLength = 140 - inputLength;
+    
+    // Change color to red if input length over limit
+    if (remainingLength < 0) {
+      $(counter).addClass("over-limit");
+    } else {
+      $(counter).removeClass("over-limit");
+    }
+    
+    $(counter).val(remainingLength);
+  })
+});
